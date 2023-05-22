926b1403d3d55217955cfd9241d5ca247fefb0af
Your Name <m.gachanga97@gmail.com>
2023-05-22 10:54:05 +0300

    first commitsdfsdfdfd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -678,7 +678,8 @@ Image credits: unsplash.com
 <script src="js/jquery.inview.min.js"></script>
 <script src="js/jquery.magnific-popup.js"></script>
 <script src="js/custom.js"></script>
-
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4317286144362168"
+        crossorigin="anonymous"></script>
 <!--main js file end-->
 </body>
 <script>
